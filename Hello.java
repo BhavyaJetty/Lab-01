4cf15991d152a1eefe6fda379102ead283d66a8e
Bhavya Jetty <bhavyajetty@Bs-MacBook-Air.local>
2021-08-25 22:09:25 -0400

    first commit upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**********************************************************************************
+* Lab 01
+***********************************************************************************
+* Bhavya Jetty 
+* 08/24/2021
+* CMSC 255 Section 004
+***********************************************************************************/
+public class Hello {
+	public static void main (String[] args) {
+
+		System.out.println("Bhavya Jetty");
+		System.out.println("Lab 01");
+		System.out.println("The first lab of the semester in order to test systems");
+		System.out.println("August 25, 2021");
+		System.out.println("CMSC 255 section 004");
+		System.out.println();
+	}
+}
